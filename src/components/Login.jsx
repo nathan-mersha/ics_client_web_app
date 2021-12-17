91d94c3cf42d147542387014e2a2cafbaad51969
Nathan <nathanmersha@gmail.com>
2021-12-17 15:05:47 +0300

    text login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,35 +1,49 @@
 import logo from '../rsr/img/logo.png'
+import icsSS from '../rsr/img/ics_ss.png'
 const Login = () => {
     return (
 
 
         <div className=" mx-auto h-screen w-full bg-gray-300 py-8">
 
-            <div className="container md:flex  ustify-center mx-auto h-full bg-gray-300">
+            <div className="container flex justify-center mx-auto h-full bg-gray-300">
 
 
-                <div className="bg-white container mx-auto flex-1 flex justify-center items-center rounded-l-xl shadow-lg flex-col" >
-                    
-                    <img className='h-16 md:h-24 md:mb-14 md:pt-3' src={logo}></img>
+                <div className="bg-white container mx-10 md:mx-auto  flex-1 flex justify-around items-center  md:rounded-l-xl shadow-lg flex-col " >
 
-                    <form>
-                        
-                        <h2 className='text-center text-gray-500 py-7 text-lg font-bold'>Login To Wallet</h2>
+                    <img className='h-24' src={logo}></img>
+
+                    <form className=''>
+                        <h2 className='text-center text-gray-500 text-lg font-bold'>Login To Wallet</h2>
+                        <div className='py-5 '></div>
                         <label className='text-gray-400 text-xs'>Phone Number</label><br />
-                        <input className='mb-5 border-b' type="text" placeholder="phone"></input><br />
+                        <input className='border-b' type="text" placeholder=""></input><br />
+                        <div className='py-2 '></div>
                         <label className='text-gray-400 text-xs'>Password</label><br />
-                        <input className='mb-7 border-b' type="text" placeholder="password"></input><br />
+                        <input className='border-b' type="text" placeholder=""></input><br />
+                        <div className='py-3'></div>
+
                         <button type="button" className="bg-black text-white container py-1 rounded shadow-lg font-bold text-sm">Login</button>
-                        <div className='flex justify-center py-5' ><h5 className='text-gray-500 text-sm pr-2'>Forgot password ? </h5> <a href="#" className='text-sm'>click here</a></div>
+                        <div className='py-2'></div>
+                        <div className='flex justify-center ' ><h5 className='text-gray-500 text-sm pr-2'>Forgot password ? </h5> <a href="#" className='text-sm'>click here</a></div>
                     </form>
                 </div>
 
 
 
-               
 
-                <div className="flex-1 rounded-r-xl shadow-lg hover:blur-sm bg-cover bg-no-repeat bg-[url('https://resources.finalsite.net/images/f_auto,q_auto/v1616523196/icsaddiseduet/nlq6phcdlrplv6t5y0wz/Optimized-ezgifcom-gif-maker1copy2.jpg')]">
-                    <div className=""></div>
+
+                <div className="hidden md:block flex-col flex-1 rounded-r-xl shadow-lg bg-yellow-200 justify-center bg-cover bg-no-repeat bg-[url('https://images.unsplash.com/photo-1605106715994-18d3fecffb98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=663&q=80')]">
+
+                    <div className='self-center'>
+                        <img className='h-72' src={icsSS}></img>
+                    </div>
+
+                    <div className=''>
+                        <h2 className='text-white text-3xl font-bold text-center'>Convenience</h2>
+                        <h4 className='text-white text-center'>Pay directly from your laptop</h4>
+                    </div>
+
                 </div>
 
 
